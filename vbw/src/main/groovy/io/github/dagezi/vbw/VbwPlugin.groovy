b74c37d607615b36df8c2b54aa69ffa3b2699afe
Takeshi Sasaki <dagezi@gmail.com>
2018-02-01 01:31:44 +0900

    Add empty plugin

---
+++ vbw/src/main/groovy/io/github/dagezi/vbw/VbwPlugin.groovy
@@ -0,0 +1,11 @@
+package io.github.dagezi.vbw
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VbwPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+
+    }
+}
